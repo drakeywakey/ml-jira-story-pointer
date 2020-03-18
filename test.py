8f2e8f6335f887ea6fc4b2583de458c7d87eca3d
walleman <will.alleman@chghealthcare.com>
2020-03-18 17:04:07 -0600

    Get predicitions going, bam!

--- test.py
+++ test.py
@@ -1,12 +1,22 @@
 from jira import JIRA
 import os
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+
+
 
 options = {
     'server': 'https://itcentral.chgcompanies.com/jira'
 }
 
-jira = JIRA(options, basic_auth=('dbennion', os.environ['JIRA_PASS']))
+jira = JIRA(options, basic_auth=('walleman', os.environ['JIRA_PASS']))
 
 # customfield_10284 = epic
 # customfield_11281 = user story
@@ -23,3 +33,36 @@ issues = {issue['key']: issue['fields'] for issue in query['issues']}
 df = pd.DataFrame.from_dict(issues, orient='index')
 
 df.replace('', inplace=True)
+
+df.rename(columns={
+    'customfield_10284': 'epic', 
+    'customfield_11281': 'user_story', 
+    'customfield_11282': 'acceptance_criteria',
+    'customfield_10182': 'story_points'}, inplace=True)
+
+df['story_points'] = df['story_points'].astype(int)
+ 
+fig = plt.figure(figsize=(8,6))
+df.groupby('story_points')['summary'].count().plot.bar(ylim=0)
+# plt.show()
+
+tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1', ngram_range=(1, 2), stop_words='english')
+features = tfidf.fit_transform(df['summary']).toarray()
+labels = df['story_points']
+# print(tfidf.get_feature_names())
+
+X_train, X_test, y_train, y_test = train_test_split(df['summary'], df['story_points'], random_state = 0)
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform(X_train)
+tfidf_transformer = TfidfTransformer()
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+model = MultinomialNB().fit(X_train_tfidf, y_train)
+
+y_pred = model.predict(count_vect.transform(X_test))
+conf_mat = confusion_matrix(y_test, y_pred)
+fig, ax = plt.subplots(figsize=(10,10))
+sns.heatmap(conf_mat, annot=True, fmt='d')
+plt.ylabel('Actual')
+plt.xlabel('Predicted')
+plt.show()
+print(conf_mat)
