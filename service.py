255f2ccd547fac3ebdc093ff96ca73f7228a3b44
dbennion <drake.bennion@chghealthcare.com>
2020-03-19 16:09:21 -0600

    use multiple features, use RandomForest

--- service.py
+++ service.py
@@ -8,8 +8,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
 import seaborn as sns
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
 
 
 load_dotenv()
@@ -25,7 +27,7 @@ jira = JIRA(options, basic_auth=(os.getenv('JIRA_USER'), os.getenv('JIRA_PASS'))
 # customfield_11282 = acceptance criteria
 # customfield_10182 = story points
 
-query = jira.search_issues('project = LTC AND issuetype = Story AND "Story Points" != null AND "Story Points" > 0  AND createdDate < 2020-01-01',
+query = jira.search_issues('project = LTC AND issuetype = Story AND "Story Points" != null AND "Story Points" >= 1 AND "Story Points" <= 5 AND createdDate < 2020-01-01',
                              json_result=True,
                              maxResults=1000,
                              fields='summary,description,customfield_10284,customfield_11281,customfield_11282,labels,customfield_10182')
@@ -34,7 +36,7 @@ issues = {issue['key']: issue['fields'] for issue in query['issues']}
 
 df = pd.DataFrame.from_dict(issues, orient='index')
 
-df.replace('', inplace=True)
+df.replace(np.nan, '', inplace=True)
 
 df.rename(columns={
     'customfield_10284': 'epic', 
@@ -48,26 +50,35 @@ fig = plt.figure(figsize=(8,6))
 df.groupby('story_points')['summary'].count().plot.bar(ylim=0)
 # plt.show()
 
+df['features'] = df['summary'] + ' ' + df['description'] + ' ' + \
+                df['user_story'] + ' ' + df['acceptance_criteria'] + \
+                df['epic']
 tfidf = TfidfVectorizer(sublinear_tf=True, min_df=5, norm='l2', encoding='latin-1', ngram_range=(1, 2), stop_words='english')
-features = tfidf.fit_transform(df['summary']).toarray()
+features = tfidf.fit_transform(df['features']).toarray()
 labels = df['story_points']
 # print(tfidf.get_feature_names())
 
-X_train, X_test, y_train, y_test = train_test_split(df['summary'], df['story_points'], random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(df['features'], df['story_points'], random_state = 0)
 count_vect = CountVectorizer()
 X_train_counts = count_vect.fit_transform(X_train)
 tfidf_transformer = TfidfTransformer()
 X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-model = MultinomialNB().fit(X_train_tfidf, y_train)
+model = RandomForestClassifier(random_state=0).fit(X_train_tfidf, y_train)
 
-# y_pred = model.predict(count_vect.transform(X_test))
-# conf_mat = confusion_matrix(y_test, y_pred)
-# fig, ax = plt.subplots(figsize=(10,10))
-# sns.heatmap(conf_mat, annot=True, fmt='d')
-# plt.ylabel('Actual')
-# plt.xlabel('Predicted')
-# plt.show()
-# print(conf_mat)
+ticklabels=['1','2','3','5']
+y_pred = model.predict(count_vect.transform(X_test))
+conf_mat = confusion_matrix(y_test, y_pred)
+fig, ax = plt.subplots(figsize=(10,10))
+sns.heatmap(conf_mat, annot=True, fmt='d', xticklabels=ticklabels, yticklabels=ticklabels)
+plt.ylabel('Actual')
+plt.xlabel('Predicted')
+print(conf_mat)
+print(accuracy_score(y_test, y_pred))
+print(precision_score(y_test, y_pred, average=None))
+print(recall_score(y_test, y_pred, average=None))
+print(f1_score(y_test, y_pred, average=None))
+
+plt.show()
 
 def predict(ticket):
     issue = jira.issue(ticket)
