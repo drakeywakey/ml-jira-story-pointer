e736076b62f5d38add180ea1b8baff06ded19bad
dbennion <drake.bennion@chghealthcare.com>
2020-03-20 10:46:49 -0600

    resampling

--- service.py
+++ service.py
@@ -12,7 +12,7 @@ from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, r
 import seaborn as sns
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-
+from sklearn.utils import resample
 
 load_dotenv()
 
@@ -27,7 +27,7 @@ jira = JIRA(options, basic_auth=(os.getenv('JIRA_USER'), os.getenv('JIRA_PASS'))
 # customfield_11282 = acceptance criteria
 # customfield_10182 = story points
 
-query = jira.search_issues('project = LTC AND issuetype = Story AND "Story Points" != null AND "Story Points" >= 1 AND "Story Points" <= 5 AND createdDate < 2020-01-01',
+query = jira.search_issues('project = LTC AND issuetype = Story AND "Story Points" != null AND "Story Points" > 0 AND "Story Points" <= 13 AND createdDate < 2020-01-01',
                              json_result=True,
                              maxResults=1000,
                              fields='summary,description,customfield_10284,customfield_11281,customfield_11282,labels,customfield_10182')
@@ -45,7 +45,21 @@ df.rename(columns={
     'customfield_10182': 'story_points'}, inplace=True)
 
 df['story_points'] = df['story_points'].astype(int)
- 
+
+# resample minority classes to match three's 179 records
+minority = [0, 5, 8, 13]
+df_majority = df[(df['story_points'] == 1) | (df['story_points'] == 2) | (df['story_points'] == 3)]
+
+for story_point in minority:
+    df_minority = df[df['story_points'] == story_point]
+    df_minority_upsampled = resample(df_minority,
+                                     replace=True,
+                                     n_samples=179,
+                                     random_state=27)
+    df_majority = pd.concat([df_majority, df_minority_upsampled])
+
+df = df_majority
+
 fig = plt.figure(figsize=(8,6))
 df.groupby('story_points')['summary'].count().plot.bar(ylim=0)
 # plt.show()
@@ -63,9 +77,9 @@ count_vect = CountVectorizer()
 X_train_counts = count_vect.fit_transform(X_train)
 tfidf_transformer = TfidfTransformer()
 X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-model = RandomForestClassifier(random_state=0).fit(X_train_tfidf, y_train)
+model = RandomForestClassifier(random_state=27).fit(X_train_tfidf, y_train)
 
-ticklabels=['1','2','3','5']
+ticklabels=sorted(df['story_points'].unique())
 y_pred = model.predict(count_vect.transform(X_test))
 conf_mat = confusion_matrix(y_test, y_pred)
 fig, ax = plt.subplots(figsize=(10,10))
